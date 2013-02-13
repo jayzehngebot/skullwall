514c090f9022ac2428a6df97d62ec9f076f0dccc
Jay Zehngebot <jay.zehngebot@gmail.com>
2013-02-12 21:08:25 -0500

    First Skullwall Kommit

---
+++ src/js/skullwall.js
@@ -0,0 +1,13 @@
+ $(document).ready(function() {
+ 
+ $("a").hover(function() {
+ 	
+ var oneThirdDocHeight = ($(document).height()/10);
+ var oneQuarterDocWidth = ($(document).width()/14);
+ 
+     $(".skull").css({"height" : oneThirdDocHeight, "width" : oneQuarterDocWidth});
+     
+
+	});
+
+ });
